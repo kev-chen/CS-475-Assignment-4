bc5ecda740c11d91e5411eed13bda4d57c9105c7
Kevin Chen <kevchen21@gmail.com>
2019-06-09 11:27:05 -0400

    Usage message on incorrect keygen.py usage

--- keygen.py
+++ keygen.py
@@ -4,16 +4,20 @@ import sys
 from Crypto.PublicKey import RSA
 
 def main():
-    privateKeyFileName = sys.argv[1]
-    publicKeyFileName = sys.argv[2]
+    try:
+        privateKeyFileName = sys.argv[1]
+        publicKeyFileName = sys.argv[2]
 
-    key = RSA.generate(1024)
+        key = RSA.generate(1024)
 
-    with open(privateKeyFileName, 'wb') as pemFile:
-        pemFile.write(key.exportKey('PEM'))
+        with open(privateKeyFileName, 'wb') as pemFile:
+            pemFile.write(key.exportKey('PEM'))
 
-    with open(publicKeyFileName, 'wb') as pemFile:
-        pemFile.write(key.publickey().exportKey('PEM'))
+        with open(publicKeyFileName, 'wb') as pemFile:
+            pemFile.write(key.publickey().exportKey('PEM'))
+
+    except IndexError:
+        print('Usage: keygen.py <private_key_file_name.pem> <public_key_file_name.pem>')
 
 
 if __name__ == '__main__':
